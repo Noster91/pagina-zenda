31aa6c61a3dfb405198b4a39be721cd1b2438085
Noster91 <noster91@gmail.com>
2016-07-18 21:27:42 -0300

    Creacion archivo .js customizado; Phone Call Button Agregado; Correccion Owl Carrousel; Varios

---
+++ scripts/my-scripts.js
@@ -0,0 +1,68 @@
+// Menu Desplegable -->
+
+$(document).ready(function($) {
+	$(".hamburger").click(function() {
+
+		$(".menu-desplegable").css("visibility", "visible");
+		$(".menu-desplegable").slideToggle(500);
+	});
+});
+
+// Carrousel Main-->
+
+
+$(document).ready(function() {
+
+	$("#owl-demo").owlCarousel({
+
+		singleItem : true,
+		navigation : false, // Show next and prev buttons
+		slideSpeed : 1,
+		paginationSpeed : 400,
+		autoPlay: 3000,
+
+
+		 // "singleItem:true" is a shortcut for:
+		 // items : 1,
+		 // itemsDesktop : false,
+		 // itemsDesktopSmall : false,
+		 // itemsTablet: false,
+		 // itemsMobile : false
+
+	});
+
+});
+
+
+// Carrousel Clientes -->
+
+$(document).ready(function() {
+
+	$("#owl-demo2").owlCarousel({
+
+		 autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+		 items : 4,
+		 itemsDesktop : [1199,3],
+		 itemsDesktopSmall : [979,3]
+
+	});
+
+});
+
+// Smooth Transition -->
+
+$(function() {
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				  $('html, body').animate({
+				    scrollTop: target.offset().top
+				  }, 1000);
+				  return false;
+			}
+		}
+	});
+});
